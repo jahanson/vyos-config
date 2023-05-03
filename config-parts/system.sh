c5252584f0aef58efdfd290485dac5cac0af50e3
Joseph Hanson <joe@veri.dev>
2023-05-03 14:56:29 -0500

    Enabling backups and swapping timeservers to time.cloudflare.com.

--- config-parts/system.sh
+++ config-parts/system.sh
@@ -18,7 +18,7 @@ set system sysctl parameter kernel.pty.max value '24000'
 # set system syslog host 10.45.0.2 port '6001'
 
 # Custom backup
-# set system task-scheduler task backup-config crontab-spec '30 0 * * *'
-# set system task-scheduler task backup-config executable path '/config/scripts/custom-config-backup.sh'
+set system task-scheduler task backup-config crontab-spec '30 0 * * *'
+set system task-scheduler task backup-config executable path '/config/scripts/custom-config-backup.sh'
 
 set system time-zone 'America/Chicago'
