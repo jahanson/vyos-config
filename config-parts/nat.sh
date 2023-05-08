297ca92860f7e2041e93a33f514d28dfbab568ca
Joseph Hanson <joe@veri.dev>
2023-05-08 05:52:02 -0500

    Removed 80/443 port forwards.

--- config-parts/nat.sh
+++ config-parts/nat.sh
@@ -1,20 +1,5 @@
 #!/bin/vbash
 
-# Forward HTTP(S) to ingress
-set nat destination rule 100 description 'HTTPS'
-set nat destination rule 100 destination port '443'
-set nat destination rule 100 inbound-interface 'eth0'
-set nat destination rule 100 protocol 'tcp'
-set nat destination rule 100 translation address '10.45.0.1'
-set nat destination rule 100 translation port '443'
-
-set nat destination rule 101 description 'HTTP'
-set nat destination rule 101 destination port '80'
-set nat destination rule 101 inbound-interface 'eth0'
-set nat destination rule 101 protocol 'tcp'
-set nat destination rule 101 translation address '10.45.0.1'
-set nat destination rule 101 translation port '80'
-
 # Forward Plex to Sting
 set nat destination rule 110 description 'PLEX'
 set nat destination rule 110 destination port '32400'
