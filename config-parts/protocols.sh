7e067b7bebc65d48155044b5d88970e203e9b1bf
Joseph Hanson <joe@veri.dev>
2023-06-14 09:41:05 -0500

    Adding new master nodes.

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -13,5 +13,14 @@ set protocols bgp neighbor 10.1.1.43 remote-as '64512'
 set protocols bgp neighbor 10.1.1.44 address-family ipv4-unicast
 set protocols bgp neighbor 10.1.1.44 description 'nahar'
 set protocols bgp neighbor 10.1.1.44 remote-as '64512'
+set protocols bgp neighbor 10.1.1.61 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.61 description 'galadriel'
+set protocols bgp neighbor 10.1.1.61 remote-as '64512'
+set protocols bgp neighbor 10.1.1.62 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.62 description 'elrond'
+set protocols bgp neighbor 10.1.1.62 remote-as '64512'
+set protocols bgp neighbor 10.1.1.63 address-family ipv4-unicast
+set protocols bgp neighbor 10.1.1.63 description 'cirdan'
+set protocols bgp neighbor 10.1.1.63 remote-as '64512'
 set protocols bgp parameters router-id '10.1.0.1'
 set protocols bgp system-as '64512'
