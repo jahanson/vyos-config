801bd951637f285301ca709bf66a76ca7f400dee
Joseph Hanson <joe@veri.dev>
2023-07-23 08:36:15 -0500

    Swapping nenya mac address.

--- config-parts/service-dhcp_server.sh
+++ config-parts/service-dhcp_server.sh
@@ -84,7 +84,7 @@ set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 range 0 s
 
 # Need to add all of the macs for the servers.
 set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping nenya ip-address '10.1.1.41'
-set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping nenya mac-address '6e:e5:a0:e6:36:28'
+set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping nenya mac-address 'c8:1f:66:10:4d:b9'
 set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping vilya ip-address '10.1.1.42'
 set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping vilya mac-address 'ce:06:3f:d5:32:be'
 set service dhcp-server shared-network-name SERVERS subnet 10.1.1.0/24 static-mapping narya ip-address '10.1.1.43'
