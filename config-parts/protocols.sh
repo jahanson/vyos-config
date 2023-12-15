fd12dafa54e210c8065b807567abdbbc2302aeb6
Joseph Hanson <joe@veri.dev>
2023-12-15 12:17:47 -0600

    BGP node description changes.

--- config-parts/protocols.sh
+++ config-parts/protocols.sh
@@ -1,6 +1,7 @@
 #!/bin/vbash
 
 # BGP configuration
+## k8s workers
 set protocols bgp neighbor 10.1.1.41 address-family ipv4-unicast
 set protocols bgp neighbor 10.1.1.41 description 'nenya'
 set protocols bgp neighbor 10.1.1.41 remote-as '64512'
@@ -8,22 +9,17 @@ set protocols bgp neighbor 10.1.1.42 address-family ipv4-unicast
 set protocols bgp neighbor 10.1.1.42 description 'vilya'
 set protocols bgp neighbor 10.1.1.42 remote-as '64512'
 set protocols bgp neighbor 10.1.1.43 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.43 description 'narya'
+set protocols bgp neighbor 10.1.1.43 description 'gollum'
 set protocols bgp neighbor 10.1.1.43 remote-as '64512'
 set protocols bgp neighbor 10.1.1.44 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.44 description 'nahar'
+set protocols bgp neighbor 10.1.1.44 description 'thror'
 set protocols bgp neighbor 10.1.1.44 remote-as '64512'
-set protocols bgp neighbor 10.1.1.45 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.45 description 'thror'
-set protocols bgp neighbor 10.1.1.45 remote-as '64512'
-set protocols bgp neighbor 10.1.1.46 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.46 description 'thrain'
-set protocols bgp neighbor 10.1.1.46 remote-as '64512'
+## k8s masters
 set protocols bgp neighbor 10.1.1.61 address-family ipv4-unicast
 set protocols bgp neighbor 10.1.1.61 description 'galadriel'
 set protocols bgp neighbor 10.1.1.61 remote-as '64512'
 set protocols bgp neighbor 10.1.1.62 address-family ipv4-unicast
-set protocols bgp neighbor 10.1.1.62 description 'elrond'
+set protocols bgp neighbor 10.1.1.62 description 'thrain'
 set protocols bgp neighbor 10.1.1.62 remote-as '64512'
 set protocols bgp neighbor 10.1.1.63 address-family ipv4-unicast
 set protocols bgp neighbor 10.1.1.63 description 'cirdan'
