c5e41d2f9f1cc989da59954d812da691c64717b1
Joseph Hanson <joe@veri.dev>
2024-01-08 15:21:49 -0600

    Added camera.

--- config-parts/service-dhcp_server.sh
+++ config-parts/service-dhcp_server.sh
@@ -136,3 +136,5 @@ set service dhcp-server shared-network-name VIDEO subnet 10.1.4.0/24 range 0 sto
 
 set service dhcp-server shared-network-name VIDEO subnet 10.1.4.0/24 static-mapping driveway-camera ip-address '10.1.4.12'
 set service dhcp-server shared-network-name VIDEO subnet 10.1.4.0/24 static-mapping driveway-camera mac-address 'ec:71:db:62:aa:e9'
+set service dhcp-server shared-network-name VIDEO subnet 10.1.4.0/24 static-mapping driveway-camera ip-address '10.1.4.13'
+set service dhcp-server shared-network-name VIDEO subnet 10.1.4.0/24 static-mapping driveway-camera mac-address 'ec:71:db:5e:46:a7'
