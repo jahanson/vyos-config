a4fb1c6fb25ea43b7162a6feae8c8344696dc60d
Joseph Hanson <joe@veri.dev>
2024-01-14 14:19:57 -0600

    re-added bind and updated jahanson.tech.

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -3,6 +3,21 @@
 # Container networks
 set container network containers prefix '10.5.0.0/24'
 
+# bind
+set container name bind cap-add 'net-bind-service'
+set container name bind image 'docker.io/internetsystemsconsortium/bind9:9.19'
+set container name bind command '/usr/sbin/named -4 -f -c /etc/bind/named.conf -u bind'
+set container name bind memory '0'
+set container name bind network containers address '10.5.0.3'
+set container name bind restart 'on-failure'
+set container name bind shared-memory '0'
+set container name bind volume config destination '/etc/bind'
+set container name bind volume config source '/config/containers/bind/config'
+set container name bind volume config mode 'ro'
+set container name bind volume cache source '/tmp/bind/cache'
+set container name bind volume cache destination '/var/cache/bind'
+set container name bind volume cache mode 'rw'
+
 # haproxy-k8s-api
 set container name haproxy-k8s-api image 'docker.io/library/haproxy:2.9.0'
 set container name haproxy-k8s-api memory '0'
@@ -12,16 +27,6 @@ set container name haproxy-k8s-api shared-memory '0'
 set container name haproxy-k8s-api volume config source '/config/containers/haproxy/config/haproxy.cfg'
 set container name haproxy-k8s-api volume config destination '/usr/local/etc/haproxy/haproxy.cfg'
 set container name haproxy-k8s-api volume config mode 'ro'
-
-# haproxy-k3s-api
-set container name haproxy-k3s-api image 'docker.io/library/haproxy:2.9.0'
-set container name haproxy-k3s-api memory '0'
-set container name haproxy-k3s-api network containers address '10.5.0.3'
-set container name haproxy-k3s-api restart 'on-failure'
-set container name haproxy-k3s-api shared-memory '0'
-set container name haproxy-k3s-api volume config source '/config/containers/haproxy-k3s/config/haproxy.cfg'
-set container name haproxy-k3s-api volume config destination '/usr/local/etc/haproxy/haproxy.cfg'
-set container name haproxy-k3s-api volume config mode 'ro'
 
 # node-exporter
 set container name node-exporter environment procfs value '/host/proc'
