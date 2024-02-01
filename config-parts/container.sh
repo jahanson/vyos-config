a27892a1202934624b5148603a02d09b14b429fd
smeagol-help <132467671+smeagol-help[bot]@users.noreply.github.com>
2024-02-01 17:12:53 +0000

    fix(container): update image docker.io/library/haproxy ( 2.9.2 → 2.9.4 )
    | datasource | package                   | from  | to    |
    | ---------- | ------------------------- | ----- | ----- |
    | docker     | docker.io/library/haproxy | 2.9.2 | 2.9.4 |

--- config-parts/container.sh
+++ config-parts/container.sh
@@ -19,7 +19,7 @@ set container name bind volume cache destination '/var/cache/bind'
 set container name bind volume cache mode 'rw'
 
 # haproxy-k8s-api
-set container name haproxy-k8s-api image 'docker.io/library/haproxy:2.9.2'
+set container name haproxy-k8s-api image 'docker.io/library/haproxy:2.9.4'
 set container name haproxy-k8s-api memory '0'
 set container name haproxy-k8s-api network containers address '10.5.0.2'
 set container name haproxy-k8s-api restart 'on-failure'
