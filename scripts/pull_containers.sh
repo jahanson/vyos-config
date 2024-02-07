7343bef3cf371aed07384e8bca146391c9d2f7e0
Joseph Hanson <joe@veri.dev>
2024-02-07 15:48:36 -0600

    simplified script.

--- scripts/pull_containers.sh
+++ scripts/pull_containers.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
-mapfile -t AVAILABLE_IMAGES < <(run show container image | awk '{ if ( NR > 1  ) { print $1 ":" $2} }')
 mapfile -t CONFIG_IMAGES < <(find /config/config-parts/ -type f -exec sed -nr "s/set container name .* image '(.*)'/\1/p" {} + | sort | uniq)
 
 for image in "${CONFIG_IMAGES[@]}"; do
-if [[ ! " ${AVAILABLE_IMAGES[*]} " =~ \ ${image}\  ]]; then
-    echo "Pulling image ${image}"
-    podman pull image "${image}"
-fi
+  echo "Pulling image ${image}"
+  podman pull "${image}"
 done
